98df805fe38dcd7445b4f1215dca3a4bbaa2fb52
technicfan <107669823+Technicfan@users.noreply.github.com>
2026-08-04 23:00:19 +0200

    fix newer versions

--- src/main/java/io/github/technicfan/letterboxed/mixins/GameRendererMixin.java
+++ src/main/java/io/github/technicfan/letterboxed/mixins/GameRendererMixin.java
@@ -7,14 +7,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.sugar.Local;
 
+//? if 26.2
+/*import net.minecraft.client.gui.Gui;*/
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import io.github.technicfan.letterboxed.Letterboxed;
 
+//? if <26.2 {
 @Mixin(GameRenderer.class)
+//?} else
+/*@Mixin(Gui.class)*/
 public class GameRendererMixin {
+    //? if 1.21.11 {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/render/GuiRenderer;render(Lcom/mojang/blaze3d/buffers/GpuBufferSlice;)V", shift = At.Shift.BEFORE))
-    private void test(CallbackInfo ci, @Local GuiGraphics guiGraphics) {
+    //?} else if 26.1 {
+    /*@Inject(method = "extractGui", at = @At("TAIL"))*/
+    //?} else if 26.2
+    /*@Inject(method = "extractRenderState", at = @At("TAIL"))*/
+    private void drawLetterboxes(CallbackInfo ci, @Local GuiGraphics guiGraphics) {
         guiGraphics.pose().pushMatrix().translate(-Letterboxed.leftOff(), -Letterboxed.topOff());
         if (Letterboxed.widthOff != 0) {
             guiGraphics.fill(0, 0, Letterboxed.leftOff(), Letterboxed.guiScaledHeight, 0xff000000);
